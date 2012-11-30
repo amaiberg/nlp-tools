d266df26ba30bd38f6e0127a3910a4406fa8efd5
Avner Maiberg <avnermaiberg@gmail.com>
2012-11-29 23:41:29 -0500

    initial commit

---
+++ bio-tagger/bin/geneTestSetEvaluator.sh
@@ -0,0 +1,8 @@
+# $1 is evaluation data
+# $2 is the name of the output model
+
+java -Xmx1800m -server -classpath \
+"output/biotagger-0.5.jar:lib/trove.jar:lib/mallet-no_gnu.jar:lib/mallet-deps.jar" \
+edu.upenn.cis.taggers.gene.GeneTrainer test \
+$1 $2
+
---
+++ bio-tagger/bin/variationTrainer.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+java  -Xmx1800m -server \
+-classpath "output/biotagger-0.5.jar:\
+lib/trove.jar:lib/mallet-no_gnu.jar:lib/mallet-deps.jar:\
+lib/maxent.jar:lib/opennlp-tools-1.0.0.jar" \
+edu.upenn.cis.taggers.variation.VariationTrainer train $1 $2 $3
+
+gzip $3
+
